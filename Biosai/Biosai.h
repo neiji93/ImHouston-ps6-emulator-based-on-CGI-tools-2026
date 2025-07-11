abd666e62455315c4ac1dc94d2f59271edb2d02d
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-07-11 23:44:48 +0200

    Create Biosai.h

---
+++ Biosai/Biosai.h
@@ -0,0 +1,11 @@
+//based on : https://github.com/neiji93/July-2025-11th---particule-conductor-cooling-and-ventilation-syst-ms-programs-and-tools-fiction-.git
+
+/)notice that gpu jamming is possible (i think di)
+
+class ckampedbutlaxzipowerbyfirdtprobetogoujamming {
+
+
+}
+
+))after that lets samole the cpu probe mathematiczlnl ketneld.
+/)zi behaviour could be a computer vision model or algorithm
